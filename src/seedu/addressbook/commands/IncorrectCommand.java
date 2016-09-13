1237fb8e8705df8846fdd7f837f85497e5ecde65
Suki Tsang <sukitcl@yahoo.com.hk>
2016-09-13 15:52:13 +0800

    Add isMutating() on IncorrectCommand

--- src/seedu/addressbook/commands/IncorrectCommand.java
+++ src/seedu/addressbook/commands/IncorrectCommand.java
@@ -11,7 +11,16 @@ public class IncorrectCommand extends Command{
     public IncorrectCommand(String feedbackToUser){
         this.feedbackToUser = feedbackToUser;
     }
-
+    
+    /**
+	 * *Check whether the data mutates
+	 * 
+	 * @return true if the data mutates
+	 */
+	public boolean isMutating() {
+		return false;
+	};
+	
     @Override
     public CommandResult execute() {
         return new CommandResult(feedbackToUser);
